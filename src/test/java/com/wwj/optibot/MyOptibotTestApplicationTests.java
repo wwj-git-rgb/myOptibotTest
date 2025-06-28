eba910f2b42f06e85c5d852678b4e9421f8ab9df
wwj <1824023567@qq.com>
2025-06-28 23:03:02 +0800

    Initial commit

---
+++ src/test/java/com/wwj/optibot/MyOptibotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwj.optibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOptibotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
