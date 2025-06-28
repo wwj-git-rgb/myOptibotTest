df83965d1e1019555e7b6698ff8969d834fbe1df
wwj <1824023567@qq.com>
2025-06-28 23:11:05 +0800

    test

--- src/main/java/com/wwj/optibot/MyOptibotTestApplication.java
+++ src/main/java/com/wwj/optibot/MyOptibotTestApplication.java
@@ -9,8 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyOptibotTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyOptibotTestApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOptibotTestApplication.class, args);
+    }
 
 }
